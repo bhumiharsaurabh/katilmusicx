8a4d35a6f8c07713c3442b122b9194ef73f960af
Sky <93299453+Skyy112@users.noreply.github.com>
2022-05-18 05:35:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ API_ID = int(getenv("API_ID", "8439761"))
 API_HASH = getenv("API_HASH", "aa3i949569078118031b99673860ede0")
 OWNER_USERNAME = getenv("OWNER_USERNAME")
 BOT_TOKEN = getenv("BOT_TOKEN","9877653742:AKR61TOx72pQEap0aHp56t9yQL5VUl-C7R0")
-BOT_USERNAME = getenv("BOT_USERNAME", "TmcmusicBot")
-BOT_NAME = getenv("BOT_NAME","Tmcmusicx")
+BOT_USERNAME = getenv("BOT_USERNAME", "musicctelebot")
+BOT_NAME = getenv("BOT_NAME","PERFECT MUSIC")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "200"))
-SESSION_NAME = getenv("SESSION_NAME", "BQCdZUPVqWdHudEeVSwMaYj2FoNB7x5LMGmFBTSe603Zo8shg0e_pxyqAJ59lxCvBblBOg-yn3nxNkzleIuv3q4poErGNQLNy2QL11BBjiZv2FK47xWn522ECLgWUg4Tnxhs_gkOZQjWkLkP1NWibeaxy0y5ufTaDS905u7ksMedBw_XBjeZHFkL4kLdRbnerhijU2UP1aJ0QfEN9nrSSDJj6NxxrFpg53nnlT4H9MXyZkGvMa9_CERrgCVlyz4bKcEOqw9RrQk6PhwnawfbkP3ESgFLT3FtBCvmZGFDTY2-1wFQzla9mK00KluI2n_266KZJqFnHLXIbR329-Yyy2ddbAMQnwA")
+SESSION_NAME = getenv("SESSION_NAME", "isi sendiri")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5364565556").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1805855691").split()))
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bhumiharsaurabh/katilmusicx",
+    "https://github.com/Skyy112/musicsky",
 )
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
